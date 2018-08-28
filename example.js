aebe356bb0451007f859ff955ad0fb323df4c6d9
lependu <lependu@protonmail.com>
2018-08-28 17:29:25 +0200

    Implements next asset handling

--- example.js
+++ example.js
@@ -5,18 +5,10 @@ const fastify = require('fastify')()
 fastify
   .register(require('./index'))
   .after(() => {
-    // Add this to serve the _next assets
-    fastify.next('/_next/*', (app, req, reply) => {
-      return app.handleRequest(req.req, reply.res)
-        .then(() => {
-          reply.sent = true
-        })
-    })
-    // Then register your routes
     fastify.next('/hello')
   })
 
-fastify.listen(3000, err => {
+fastify.listen(3000, '0.0.0.0', err => {
   if (err) throw err
   console.log('Server listenging on http://localhost:3000')
 })
